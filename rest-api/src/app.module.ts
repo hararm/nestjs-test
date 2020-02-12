e3817197a845bb1b887975c3e5db01e230919bf2
Armen Harutyunyan <armen@addatech.ca>
2020-02-12 17:38:36 -0500

    middleware added

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {CoursesModule} from './courses/courses.module';
 import {MongooseModule} from '@nestjs/mongoose';
 import {MONGO_CONNECTION} from './constants';
 import {AuthModule} from './auth/auth.module';
+import {GetUserMiddleware} from './middleware/get-user.middleware';
+import {CoursesController} from './courses/controllers/courses.controller';
+import {LessonsController} from './courses/controllers/lessons.controller';
 
 @Module({
-  imports: [AuthModule, CoursesModule, MongooseModule.forRoot(MONGO_CONNECTION)],
-  providers: [],
+    imports: [AuthModule, CoursesModule, MongooseModule.forRoot(MONGO_CONNECTION)],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+        consumer
+            .apply(GetUserMiddleware)
+            .forRoutes(
+                CoursesController,
+                LessonsController
+            );
+
+    }
+}
